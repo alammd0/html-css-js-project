1d2ac3ef440c4edb1520a36d4c92d88eb44a0478
Md Khalid Alam <sonualam96314@gmail.com>
2024-09-28 08:51:41 +0530

    lazy-effect add

--- e-architech/app.js
+++ e-architech/app.js
@@ -11,3 +11,48 @@ toggle_btn.addEventListener("click", function(err){
     toggle_menu.classList.toggle("active");
 
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyElements = document.querySelectorAll('.lazy-show');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry, index) => {
+            if (entry.isIntersecting) {
+                setTimeout(() => {
+                    entry.target.classList.add('show');
+                }, index * 200); 
+                
+                observer.unobserve(entry.target);  
+            }
+        });
+    }, { threshold: 0.1 });
+
+    lazyElements.forEach(el => {
+        observer.observe(el);
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyElementsLeft = document.querySelectorAll('.lazy-show-left');
+    const lazyElementsRight = document.querySelectorAll('.lazy-show-right');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    lazyElementsLeft.forEach(el => {
+        observer.observe(el);
+    });
+
+    lazyElementsRight.forEach(el => {
+        observer.observe(el);
+    });
+});
+
