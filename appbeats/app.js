3cea9cd2812652271a9276b5984a5c214845c47f
Md Khalid Alam <sonualam96314@gmail.com>
2024-10-13 11:49:22 +0530

    add more section

--- appbeats/app.js
+++ appbeats/app.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
         navBar.classList.toggle('active'); // Toggle the active class
     });
 });
+
+
+function selectAndchange(element, contentId){
+    // remove all active classes
+    document.querySelectorAll('.tech-item').forEach(item => {
+        item.classList.remove('active');
+    })
+
+    // then add the new class on selected element 
+    element.classList.add('active');
+
+    // remove all active classes from content_item
+    document.querySelectorAll('.tech_content_item').forEach(item => {
+        item.classList.remove('active');
+    })
+
+    // add the new class on selected element
+    document.getElementById(contentId).classList.add('active');
+}
